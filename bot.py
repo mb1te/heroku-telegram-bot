4e56cbd55e00ee877106a8801529abd277608444
mb1te <mb1te.comics@gmail.com>
2019-01-30 14:11:50 +0300

    fact

--- bot.py
+++ bot.py
@@ -1,11 +1,12 @@
 import vk_api
 import math
+import random
 from vk_api.longpoll import VkLongPoll, VkEventType
 
-token =  '636b5ca075a225c1ae9ae8f36071ea8b1e3a562aab156c4c2095ead56ea3a16d251ccd708f2197eccd755'
+token = '636b5ca075a225c1ae9ae8f36071ea8b1e3a562aab156c4c2095ead56ea3a16d251ccd708f2197eccd755'
 
 def write_msg(user_id, message):
-    vk.method('messages.send', {'user_id' : user_id, 'message' : message})
+    vk.method('messages.send', {'user_id' : user_id, 'message' : message, 'random_id': random.randint(1, 10**12)})
 
 vk = vk_api.VkApi(token=token)
 
