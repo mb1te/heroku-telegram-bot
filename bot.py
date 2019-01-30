7254663eb999554862df3c5fb3203a7708dbc167
mb1te <mb1te.comics@gmail.com>
2019-01-30 19:08:37 +0300

    расписание звонков

--- bot.py
+++ bot.py
@@ -1,9 +1,10 @@
 import vk_api
 import math
 import random
+from setings import *
+from data import *
 from vk_api.longpoll import VkLongPoll, VkEventType
 
-token = '636b5ca075a225c1ae9ae8f36071ea8b1e3a562aab156c4c2095ead56ea3a16d251ccd708f2197eccd755'
 
 def write_msg(user_id, message):
     vk.method('messages.send', {'user_id' : user_id, 'message' : message, 'random_id': random.randint(1, 10**12)})
@@ -16,11 +17,5 @@ for event in longpoll.listen():
     if event.type == VkEventType.MESSAGE_NEW:
         if event.to_me:
             request = event.text
-            if "#factorial " in request:
-                s = request.split()
-                try:
-                    write_msg(event.user_id, str(math.factorial(int(s[1]))))
-                except:
-                    write_msg(event.user_id, "sorry")
-            else:
-                write_msg(event.user_id, "sorry")
+            if "расписание звонков" in request:
+                write_msg(event.user_id, schedule)
