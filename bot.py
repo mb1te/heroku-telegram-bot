5dd32623b3758bed0414d7c047dff82975645abb
mb1te <45900239+mb1te@users.noreply.github.com>
2019-01-30 13:33:05 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,20 +1,27 @@
 # -*- coding: utf-8 -*-
-import redis
-import os
-import telebot
-# import some_api_lib
-# import ...
-
-# Example of your code beginning
-#           Config vars
 token = os.environ['TELEGRAM_TOKEN']
 some_api_token = os.environ['SOME_API_TOKEN']
-#             ...
 
-# If you use redis, install this add-on https://elements.heroku.com/addons/heroku-redis
-r = redis.from_url(os.environ.get("REDIS_URL"))
+import vk_api
+from vk_api.longpoll import VkLongPoll, VkEventType
 
-#       Your bot code below
-# bot = telebot.TeleBot(token)
-# some_api = some_api_lib.connect(some_api_token)
-#              ...
+token =  '636b5ca075a225c1ae9ae8f36071ea8b1e3a562aab156c4c2095ead56ea3a16d251ccd708f2197eccd755'
+
+def write_msg(user_id, message):
+    vk.method('messages.send', {'user_id' : user_id, 'message' : message})
+
+vk = vk_api.VkApi(token=token)
+
+longpoll = VkLongPoll(vk)
+
+for event in longpoll.listen():
+    if event.type == VkEventType.MESSAGE_NEW:
+        if event.to_me:
+            request = event.text
+            if request == "Привет":
+                write_msg(event.user_id, "Hi there!")
+            elif request == "Пока":
+                write_msg(event.user_id, "Good bye(((")
+            else:
+                write_msg(event.user_id, "I didn't understood!")
+
